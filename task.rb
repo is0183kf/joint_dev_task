3417b32f4e8f4224d6f04001099a0aafdb02efd1
YuudaiHiguchi <is0183kf@gmail.com>
2020-08-25 22:14:34 +0900

    Q9 to Q16

--- task.rb
+++ task.rb
@@ -75,28 +75,36 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do | name, i|
+    puts "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+    puts food.include?("うに")? "#{food}:好物です" : "#{food}:まぁまぁ好きです"
+  end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  puts "ユーザーの趣味一覧"
+  sports.flatten!.uniq!
+  sports.each.with_index(1) do | sport, i|
+    puts "No#{i} #{sport}"
+  end
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  p data[:user][:name]
 end
 
 def q13
@@ -104,14 +112,14 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  p user_data.update(update_data)
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  p data2 = data.keys
 end
 
 def q15
@@ -119,7 +127,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  puts data1.include?(:age) ? "OK" : "NG"
+  puts data2.include?(:age) ? "OK" : "NG"
 end
 
 def q16
@@ -131,7 +140,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.map do | user |
+    puts "私の名前は#{user[:name]} です。年齢は#{user[:age]}歳です。"
+  end 
 end
 
 class UserQ17
